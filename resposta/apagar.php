2b0f82d4863bbe2f69de349ce03f112ee663e042
Marcelo Matos <marcelo.matos@ufrr.br>
2016-11-25 16:48:04 -0400

    Adicionado link para apagar resposta

--- resposta/apagar.php
+++ resposta/apagar.php
@@ -3,7 +3,12 @@
   
   $conexao->conecta();
   
+  $conexao->begin();
+  
+  $conexao->query("DELETE FROM dados WHERE idresposta=".intval($_GET['id']));
   $conexao->query("DELETE FROM respostas WHERE idresposta=".intval($_GET['id']));
+
+  $conexao->commit();
   
   $conexao->desconecta();
   
